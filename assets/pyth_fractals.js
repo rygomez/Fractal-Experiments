0c796223293c0ffbb11e840c5ddb798f674d0dd3
System Administrator <root@Gobot-Alpha.local>
2014-11-29 20:21:15 -0500

    adding simple fractals

---
+++ assets/pyth_fractals.js
@@ -0,0 +1,17 @@
+
+window.addEvent('domready',function(){
+	var i,f,
+		fractals = [
+			{	name: 'pythagoras',
+				constructor: Pythagoras,
+				width:800,
+				height:530 }
+		];
+	
+	// setup th rest of the fractal stuff.
+	for(i=0; i<fractals.length; i++){
+		// actually instantiate each fractal.
+		f = fractals[i];
+		f.instance = new f.constructor(f.name+"_canvas");
+	}// end for
+});
